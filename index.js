f0b064b2136b9dc9c576149acfd96218d3f748de
Brandon <hillbp2014@gmail.com>
2022-10-24 18:00:04 -0400

    did some stuff

--- index.js
+++ index.js
@@ -4,15 +4,87 @@ const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of the project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter a brief description of the project",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "How does someone install the project?",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "What are the directions for using the project?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "How does someone contribute to the project?",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "How does someone run tests for the project?",
+        name: "test"
+    },
+    {
+        type: "list",
+        message: "Which license will the project use?",
+        name: "license",
+        choices: ["MIT License", "Apache License 2.0", "BSD License", "GPL License", "Mozilla Public License 2.0"]
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
+    fs.writeFileSync(fileName, data);
+    console.log(`Created file ${fileName} successfully.`);
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            let content = generateMarkdown(answers);
+            let filename = "README.md";
+            let isDuplicateFile;
+            let i = 1;
+            do {
+                try {
+                    fs.accessSync(filename, fs.constants.F_OK);
+                    console.log('File already exists');
+                    isDuplicateFile = true;
+                    filename = `README(${i++}).md`;
+                  } catch (err) {
+                    console.error('File does not exist');
+                    isDuplicateFile = false;
+                  }
+            } while (isDuplicateFile);
+            writeToFile(filename, content);
+        })
+        .catch((error) => {
+            console.error(error.message);
+        })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    // TODO: Implement
+    return "License Badge";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    // TODO: Implement
+    return "License Link";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    // TODO: Implement
+    return "License Description";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
-
+${renderLicenseBadge(data.license)}
 ## Description
 
 ${data.description}
@@ -22,22 +31,18 @@ ${data.description}
 
 - [Installation](#installation)
 - [Usage](#usage)
-- [Credits](#credits)
 - [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
 ## Installation
 
-To install this application, run the following command:
-
-    ${data.install}
+${data.install}
 
 ## Usage
 
-${data.instructions}
-
-## Credits
-
-- ${data.author}
+${data.usage}
 
 ## License
 
@@ -45,12 +50,21 @@ ${renderLicenseBadge(data.license)}
 ${renderLicenseLink(data.license)}
 ${renderLicenseSection(data.license)}
 
+## Contributing
+
+${data.contributing}
+
 ## Tests
 
 To run tests for this application, run the following command:
 
     ${data.test}
 
+## Questions
+
+If you have questions, please contact me at:
+
+[${data.username}](https://github.com/${data.username}) \([Email](mailto:${data.email}\)
 `;
 }
 
